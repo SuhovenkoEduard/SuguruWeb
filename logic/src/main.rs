79d398012413a67be586019632abee611a208f46
SuhovenkoEduard <avatar_dj@mail.ru>
2022-07-11 12:10:08 +0300

    guessing game Reading rust documentation (chapter 1-2)

--- logic/src/main.rs
+++ logic/src/main.rs
@@ -1,3 +1,37 @@
+use std::{io, cmp::Ordering};
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret number is {secret_number}");
+    
+    loop {
+        println!("Please input your guess.");
+        
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Readline failed.");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Couldn't parse guess number. {err}");
+                continue;
+            },
+        };
+
+        println!("Our guess is {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Less"),
+            Ordering::Equal => {
+                println!("Equal");
+                break;
+            },
+            Ordering::Greater => println!("Greater"),
+        }   
+    }
 }
